ce7698c24ae55d52332f6d6d8c7192ac5c659581
productdevbook <hi@productdevbook.com>
2023-03-06 07:59:54 +0300

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export function handler(event: any, context: any, callback: any) {
-  console.log('Hello World')
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello World',
-  })
-}
+export { makeAsyncStorage } from './makeAsyncStorage';
